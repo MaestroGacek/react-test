284f318c4dd76aaf81cbcedd5aaf255ce279a720
MaestroGacek <kohout.jan@hotmail.cz>
2024-03-27 09:20:44 +0100

    Barvičky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
-import Home from "./Home"; 
+import Home from "./Home";
 import Layout from "./Layout";
 import Cafe from "./Cafe";
 import Kalkulacka from "./Kalkulacka/Kalkulacka";
@@ -12,32 +12,34 @@ import Ukolnicek from "./Ukolnicek/Ukolnicek";
 import Eshop from "./E-shop/Eshop";
 import Eshop2 from "./E-shop2/Eshop2";
 import Eshop3 from "./Eshop3";
+import Barva from "./Barva/Barva"
+import { useState } from "react";
 
 function App() {
-  
+  const [barva, nastavBarvu] = useState('')
   
   return (
     <>
-    <HashRouter basename='/'>
-    <Routes>
-    <Route path="/" element={<Layout></Layout>} >
-    <Route index element={<Home></Home>}/>
-    <Route path="/cafe" element={<Cafe></Cafe>}/>
-    <Route path="/kalkulacka" element={<Kalkulacka></Kalkulacka>}/>
-    <Route path="/pocitadlo" element={<Pocitadlo></Pocitadlo>}/>
-    <Route path="/pocitadlo2" element={<Pocitadlo2></Pocitadlo2>}/>
-    <Route path="/auta" element={<Auta></Auta>}/>
-    <Route path="/profil" element={<Profil></Profil>}/>
-    <Route path="/prumer" element={<Prumer></Prumer>}/>
-    <Route path="/ukolnicek" element={<Ukolnicek></Ukolnicek>}/>
-    <Route path="/eshop" element={<Eshop></Eshop>}></Route>
-    <Route path="/eshop2" element={<Eshop2></Eshop2>}></Route>
-    <Route path="/eshop3" element={<Eshop3></Eshop3>}></Route>
-</Route>
-   </Routes>
-    </HashRouter>
+      <HashRouter basename='/'>
+        <Layout barva={barva}/>
+        <Routes>
+            <Route index element={<Home></Home>} />
+            <Route path="/cafe" element={<Cafe></Cafe>} />
+            <Route path="/kalkulacka" element={<Kalkulacka></Kalkulacka>} />
+            <Route path="/pocitadlo" element={<Pocitadlo></Pocitadlo>} />
+            <Route path="/pocitadlo2" element={<Pocitadlo2></Pocitadlo2>} />
+            <Route path="/auta" element={<Auta></Auta>} />
+            <Route path="/profil" element={<Profil></Profil>} />
+            <Route path="/prumer" element={<Prumer></Prumer>} />
+            <Route path="/ukolnicek" element={<Ukolnicek></Ukolnicek>} />
+            <Route path="/eshop" element={<Eshop></Eshop>}></Route>
+            <Route path="/eshop2" element={<Eshop2></Eshop2>}></Route>
+            <Route path="/eshop3" element={<Eshop3></Eshop3>}></Route>
+            <Route path="/barva" element={<Barva nastavBarvu={nastavBarvu} barva={barva}></Barva>}></Route>
+        </Routes>
+      </HashRouter>
 
-  
+
     </>
   )
 
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material";
 export default function Home(){
+    
     return(
         <>
         <Typography>
-        <h1>Portfólio nejlepších projektů v Reactu</h1>
+        <h1 >Portfólio nejlepších projektů v Reactu</h1>
         <img src="profilovka.jpg" ></img>
         </Typography>
         </>
